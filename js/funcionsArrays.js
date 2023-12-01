2910f8972005e080f4d13802f1fa0d015cd4eb7b
Jordi Sabroson <2023_jordi.manya@iticbcn.cat>
2023-12-01 16:18:16 +0100

    progrés

--- js/funcionsArrays.js
+++ js/funcionsArrays.js
@@ -4,5 +4,25 @@
  */
 
 function funcionsArrays() {
-    
+    // Array d'exemple
+    let array = ['hola', 'salutacions', 'benaventurat', 'uep', 'com va', 'que pasa', 'osti', 'pilotes'];
+    let array2 = ['un', 'dos'];
+    document.write("Array d'exemple: " + array + "<br>");
+
+    // at() Returns an indexed element of an array
+    document.write("at(): " + array.at(1) + "<br>");
+
+    // concat() Joins arrays and returns an array with the joined arrays
+    document.write("concat(): " + array.concat(array2) + "<br>");
+
+    // constructor Returns the function that created the Array object's prototype
+    arr = new Array('1','2');
+    document.write("constructor: " + arr + "<br>");
+
+    // copyWithin() Copies array elements within the array, to and from specified positions
+    document.write("copyWithin(): " + array.copyWithin(0,4) + "<br>");
+
+    // entries() Returns a key/value pair Array Iteration Object
+    let iterador = array.entries();
+    document.write("entries(): " + iterador.next().value + "<br>");
 }
