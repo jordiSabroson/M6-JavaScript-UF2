782043ed6937a02144fe18228c19452c094a5b9a
Jordi Sabroson <2023_jordi.manya@iticbcn.cat>
2024-01-17 19:46:58 +0100

    avenç

--- Projecte Arrays/js/script.js
+++ Projecte Arrays/js/script.js
@@ -1,146 +1,182 @@
+let dades = null;
+let llista = [];
+let noms = [];
 
-// POKEMONS
-let dadesPokemons, dadesMunicipis, dadesMeteorits, dadesPelis;
+function triarJSON() {
+	event.preventDefault();
+	dades = null;
+	llista = [];
+	noms = [];
+	let valor = document.querySelector('input[value="pokemons"]:checked') ||
+		document.querySelector('input[value="municipis"]:checked') ||
+		document.querySelector('input[value="meteorits"]:checked') ||
+		document.querySelector('input[value="movies"]:checked');
 
-// ARRAYS
-let pokemons = [], municipis = [], meteorits = [], movies = [], llista = [];
+	let bbdd = valor.value;
+	if (bbdd == "pokemons") {
+		// POKEMONS
+		fetch("js/data/pokemon.json")
+			.then((response) => response.json())
+			.then((data) => {
+				dades = data.pokemon;
 
-// POKEMONS
-fetch("js/data/pokemon.json")
-	.then((response) => response.json())
-	.then((data) => {
-		dadesPokemons = data.pokemon;
+				// Bucle per guardar els noms dels pokemons a un array
+				for (let i in dades) {
+					noms.push(dades[i].name)
+				}
+				console.log("Noms dels pokemons: " + noms.join(", "));
 
-		//console.log(dades)
-		for (let i in dadesPokemons) {
-			//console.log(dades[i].name)
-			pokemons.push(dadesPokemons[i].name)
-		}
+				// Codi per guardar en un array anomenat "llista" només certes dades per mostrar en una taula al DOM
+				dades.forEach((pokemon) => {
+					let pokemonSenseKG = parseFloat(pokemon.weight);
+					llista.push([pokemon.id, pokemon.name, pokemon.img, pokemonSenseKG]);
+				});
 
-		dadesPokemons.forEach((pokemon) => {
-			let pokemonSenseKG = parseFloat(pokemon.weight);
-			llista.push([pokemon.id, pokemon.name, pokemon.img, pokemonSenseKG]);
-		});
-	});
+				// Imprimir l'array anterior en forma de taula per consola
+				console.table(llista);
+			});
+	} else if (bbdd == "municipis") {
+		// MUNICIPIS
+		fetch("js/data/municipis.json")
+			.then((response) => response.json())
+			.then((data) => {
+				dades = data.elements;
 
-// MUNICIPIS
-fetch("js/data/municipis.json")
-	.then((response) => response.json())
-	.then((data) => {
-		dades = data.elements;
+				for (let i in dades) {
+					noms.push(dades[i].municipi_nom)
+				}
+				console.log("Noms dels municipis: " + noms.join(", "));
+				dades.forEach((municipi) => {
+					llista.push([municipi.ine, municipi.municipi_nom, municipi.municipi_bandera]);
+				});
+				console.table(llista);
+			});
+	} else if (bbdd == "meteorits") {
+		// METEORITS
+		fetch("js/data/earthMeteorites.json")
+			.then((response) => response.json())
+			.then((data) => {
+				dades = data;
+				for (let i in dades) {
+					noms.push(dades[i].name)
+				}
+				console.log("Noms dels meteorits: " + noms.join(", "));
 
-		console.log(dades)
-		for (let i in dades) {
-			console.log(dades[i].municipi_nom)
-			municipis.push(dades[i].municipi_nom)
-		}
-	});
+				dades.forEach((meteorit) => {
+					llista.push([meteorit.id, meteorit.name, meteorit.mass]);
+				});
+				console.table(llista);
+			});
+	} else if (bbdd == "movies") {
+		// MOVIES
+		fetch("js/data/movies.json")
+			.then((response) => response.json())
+			.then((data) => {
+				dades = data.movies;
 
+				for (let i in dades) {
+					noms.push(dades[i].title)
+				}
+				console.log("Noms de les pel·lícules: " + noms.join(", "));
 
-
-// // METEORITS
-// fetch("js/data/earthMeteorites.json")
-// 	.then((response) => response.json())
-// 	.then((data) => {
-// 		dades = data;
-
-// 		console.log(dades)
-// 		for (let i in dades) {
-// 			console.log(dades[i].name)
-// 			meteorits.push(dades[i].name)
-// 		}
-// 	});
-
-
-// // MOVIES
-// fetch("js/data/movies.json")
-// 	.then((response) => response.json())
-// 	.then((data) => {
-// 		dades = data.movies;
-
-// 		console.log(dades)
-// 		for (let i in dades) {
-// 			console.log(dades[i].title)
-// 			movies.push(dades[i].title)
-// 		}
-// 	});
+				dades.forEach((movies) => {
+					llista.push([movies.title, movies.rating, movies.url, movies.year]);
+				});
+				console.table(llista);
+			});
+	}
+}
 
 
 function orderList(ordre) {
 	if (ordre == "asc") {
-		pokemons.sort();
+		noms.sort();
 	} else if (ordre == "desc") {
-		pokemons.reverse();
+		noms.reverse();
 	}
-	console.log(pokemons)
+	console.log(noms)
 }
 
 function searchList(index) {
-	index = parseInt(prompt("introdueix un index per buscar a l'array [0 - " + pokemons.length + "]"));
+	index = parseInt(prompt("introdueix un index per buscar a l'array [0 - " + noms.length + "]"));
 	if (isNaN(index)) {
 		console.log("No has introduït cap número.");
 		return;
 	}
-	console.log("Pokémon amb índex número " + index + ": " + pokemons[index]);
+	console.log("Element número " + index + ": " + noms[index]);
 }
 
 function calcMitjana() {
-	let pesTotal = 0, c = 0;
-	dadesPokemons.forEach((pokemon) => {
-		pesTotal += parseFloat(pokemon.weight);
-		c++;
-	});
-	let mitjana = pesTotal / c;
+	let total = 0, c = 0;
+	if (bbdd == "pokemons") {
+		dades.forEach((item) => {
+			total += parseFloat(item.weight);
+			c++;
+		});
+	} else if (bbdd == "municipis") {
+		alert("No hi ha valors amb els que calcular una mitjana!");
+	} else if (bbdd == "meteorits") {
+		dades.forEach((item) => {
+			total += parseFloat(item.mass);
+			c++;
+		});
+	} else if (bbdd == "movies") {
+		dades.forEach((item) => {
+			total += parseFloat(item.rating);
+			c++;
+		});
+	}
+	let mitjana = total / c;
 	alert(mitjana.toFixed(2));
 }
 
 function printList() {
-	let div = document.getElementById("resultat");
-	let table = document.createElement("table");
-	let tbody = document.createElement("tbody");
-	
-	let filaTitols = document.createElement("tr");
-	let titols = ["ID", "NOM", "IMATGE", "PES"];
-	titols.forEach(titol => {
-		let th = document.createElement("th");
-		th.textContent = titol;
-		filaTitols.appendChild(th);
-	});
-	tbody.appendChild(filaTitols);
+	if (dades == null) {
+		alert("Selecciona una base de dades abans!");
+	} else {
+		let div = document.getElementById("resultat");
+		let table = document.createElement("table");
+		let tbody = document.createElement("tbody");
+		let filaTitols = document.createElement("tr");
+		let titols = ["ID", "NOM", "IMATGE", "PES"];
+		titols.forEach(titol => {
+			let th = document.createElement("th");
+			th.textContent = titol;
+			filaTitols.appendChild(th);
+		});
+		tbody.appendChild(filaTitols);
 
-	for (let pokemon of llista) {
-		let fila = document.createElement("tr");
+		for (let item of llista) {
+			let fila = document.createElement("tr");
 
-		// ID
-		let casellaID = document.createElement("td");
-		casellaID.textContent = pokemon[0];
-		fila.appendChild(casellaID);
+			// ID
+			let casellaID = document.createElement("td");
+			casellaID.textContent = item[0];
+			fila.appendChild(casellaID);
 
-		// Nom
-		let casellaNom = document.createElement("td");
-		casellaNom.textContent = pokemon[1];
-		fila.appendChild(casellaNom);
-	  
-		// Imatge
-		//let img = document.createElement("img");
-		let casellaImatge = document.createElement("td");
-		casellaImatge.innerHTML = "<img src='" + pokemon[2] +"'></img>";
-		
-		//casellaImatge.appendChild(img);
-		fila.appendChild(casellaImatge);
-	  
-		// Pes
-		let casellaPes = document.createElement("td");
-		casellaPes.textContent = pokemon[3];
-		fila.appendChild(casellaPes);
-		
-		tbody.appendChild(fila);
+			// Nom
+			let casellaNom = document.createElement("td");
+			casellaNom.textContent = item[1];
+			fila.appendChild(casellaNom);
+
+			// Imatge
+			//let img = document.createElement("img");
+			let casellaImatge = document.createElement("td");
+			casellaImatge.innerHTML = "<img src='" + item[2] + "'></img>";
+
+			//casellaImatge.appendChild(img);
+			fila.appendChild(casellaImatge);
+
+			// Pes
+			let casellaPes = document.createElement("td");
+			casellaPes.textContent = item[3];
+			fila.appendChild(casellaPes);
+
+			tbody.appendChild(fila);
+		}
+
+		table.appendChild(tbody);
+		div.appendChild(table);
+		document.body.appendChild(table);
 	}
-
-	table.appendChild(tbody);
-	div.appendChild(table);
-	document.body.appendChild(table);
-
 }
-
-console.log(llista);
